2f64845d2b21d8dedf42071258c63c1fc89a21ab
aeyy <davimraba2020@gmail.com>
2026-05-20 13:45:33 +0900

    update category and product

--- src/category.py
+++ src/category.py
@@ -11,7 +11,7 @@ class Category:
 
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
     def __init__(self, name: str, description: str, products: list[Product]):
         """
@@ -22,7 +22,17 @@ class Category:
         """
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        return "".join(
+            f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт. \n" for product in self.__products
+        )
--- src/product.py
+++ src/product.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Product:
     """
     Product class
@@ -5,7 +8,7 @@ class Product:
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
@@ -20,3 +23,18 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, p_dict: dict) -> Product:
+        return cls(**p_dict)
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, value: float) -> None:
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
